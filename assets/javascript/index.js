a0fc0b8c11df183bfa20f7b3fbf0345f1ac53d96
MATT0568 <lesniewiczm@vcu.edu>
2019-01-09 01:05:15 -0500

    added similar recipes and fixed submit

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,5 @@
+var ingredient_string = "";
+
 $("#ingredient").on("keyup", function () {
   var ingredient = $("#ingredient").val().trim();
   if (ingredient.length > 2) {
@@ -29,10 +31,10 @@ $("#submit-ingredient").on("click", function (event) {
   if (ingredient.includes("%")) {
     url_ingredient = ingredient.replace(/%/g, "%25");
   }
-  var url = "https://apibeta.nutritionix.com/v2/search?q=" + url_ingredient + "&appId=5046f269&appKey=b98cd96564773ae253d3510e0f580572";
+  var ingredient_url = "https://apibeta.nutritionix.com/v2/search?q=" + url_ingredient + "&appId=5046f269&appKey=b98cd96564773ae253d3510e0f580572";
 
   $.ajax({
-    url: url,
+    url: ingredient_url,
     method: "GET"
   }).then(function (response) {
     var calories = response.results[0].nutrient_value + " per " + response.results[0].serving_uom;
@@ -40,8 +42,23 @@ $("#submit-ingredient").on("click", function (event) {
     if (ingredient !== "" && amount !== "") {
       $("#rows").append("<tr><td>" + ingredient + "</td><td>" + amount + "</td><td>" + calories + "</td><tr>");
     }
-  
+
     $("#ingredient").val("");
+  });
+  if (ingredient_string === "") {
+    ingredient_string = ingredient;
+  }
+  else {
+    ingredient_string = ingredient_string + "&q=" + ingredient;
+  }
+  var recipe_url = "https://api.edamam.com/search?q=" + ingredient_string + "&app_id=5452f6ef&app_key=28579f5271bcb880b21ca0931120f5d3";
+  console.log(recipe_url);
+
+  $.ajax({
+    url: recipe_url,
+    method: "GET"
+  }).then(function (response) {
+    popSimilar(response);
   });
 });
 
@@ -49,21 +66,46 @@ $("#submit-recipe").on("click", function () {
   console.log("hi");
   var data = getTableData();
   var recipe_name = $("#recipe-name").val().trim();
+  console.log(data);
   $.post("submit.php", { data: data, recipe_name: recipe_name },
-  // function(data) {
-  //   $('#results').append(data);
-  // }
+    // function(data) {
+    //   $('#results').append(data);
+    // }
   );
 });
 
 function getTableData() {
   var data = [];
   $("#table").find('tr').each(function (rowIndex, r) {
-      var cols = [];
-      $(this).find('th,td').each(function (colIndex, c) {
-          cols.push(c.textContent);
-      });
+    var cols = [];
+    $(this).find('th,td').each(function (colIndex, c) {
+      cols.push(c.textContent);
+    });
+    if (cols.length !== 0) {
       data.push(cols);
+    }
   });
   return data;
+}
+
+function popSimilar(response) {
+  $("#similar").html("");
+  for (var i = 0; i < 5; i++) {
+    var recipe = response.hits[i].recipe.label;
+    var image_url = response.hits[i].recipe.image;
+    var url = response.hits[i].recipe.url;
+    var ingredients = response.hits[i].recipe.ingredientLines;
+    console.log(url);
+    $("#similar").append(
+      "<div class='card' style='width: 18rem;'>" +
+      "<img class='card-img-top' src=" + image_url + " alt='Card image cap' style='height: 10rem'>" +
+      "<div class='card-body'>" +
+      "<h5 class='card-title'><a href=" + url + "><span>" + recipe + "</span></a></h5>" +
+      "</div>" +
+      "</div>"
+    );
+    // for (var i = 0; i < response.hits.length(); i++) {
+
+    // }
+  }
 }
