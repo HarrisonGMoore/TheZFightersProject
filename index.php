3db1e166e9b81d034b3c4327c63a5403d75900e2
HarrisonGMoore <43092861+HarrisonGMoore@users.noreply.github.com>
2019-01-11 14:16:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,42 +9,34 @@
     
     <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css">
     <script src="main.js"></script>
+    <?php include('validate.php'); ?>
+    <?php include('..connect.php'); ?>
 </head>
 <body>
-<div class="container-fluid">
-    <div class="container-fluid" id="header-footer">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <img class="mr-3" src="assets/recipe-builder-logo.png" alt="Recipe Builder" id="logo">
+<?php include 'connect.php'; ?>
+<div class="container-fluid" id="header-footer">
+        <nav class="navbar navbar-expand-lg">
+            <img class="mr-3" src="assets/css/recipe-builder-logo.png" alt="Recipe Builder" id="logo">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-        
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                <a class="nav-link" href="#">Create a New Recipe<span class="sr-only"></span></a>
-                </li>
-                <li class="nav-item active">
-                <a class="nav-link" href="">My Recipes</a>
-                </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <a class="nav-link" href="#">Login<span class="sr-only"></span></a>
-                <a class="nav-link" href="#">Signup<span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="create-user.php">Not a member? Sing up here:<span class="sr-only"></span></a>
             </form>
             </div>
         </nav>
     </div>
 
     <!-- MAIN PAGE CONTENT GOES INBETWEEN HERE -->
-    <div id="login-box">
-        <form>
+    <div>
+        <form method="post" id="login-box">
         Email:
         <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="jsmith@johnsmith.com">
         Password:
-        <input type="email" class="form-control" name="password" id="password" aria-describedby="emailHelp" placeholder="Don't share it!">
+        <input type="password" class="form-control" name="password" id="password"  placeholder="password">
         <br>
-        <button name= "Submit" type="submit" class="btn btn-primary">Submit</button>
+        <button name="Submit" type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
     <?php
@@ -68,30 +60,24 @@
           $perm->execute() or die($perm->error);
           $fields = $perm->get_result();
           $user_id = -1;
-
           while ($row = $fields->fetch_assoc()) {
             $user_id = $row["user_id"];
           }
-
           $_SESSION["USER_ID"] = $user_id;
-
           //Redirect to the correct page
           header('Location: /user/index.php');
-
         }
         else {
           ?><div class="row" style="padding-top: 1rem"><div class="col-md-6"></div><div class="col-md-6"><?php print 'Invalid email/password. Please try again.';?></div></div><?php
         }
     }
-
-
     ?>
     <!-- MAIN PAGE CONTENT ENDS HERE -->
 
     <div class="container-fluid fixed-bottom" id="header-footer">
         <footer class="page-footer font-small">
             <div class="footer-copyright text-center py-3">© 2018 Copyright:
-            <a href="">TheZFighters</a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">TheZFighters</a>
             </div>
         </footer>
     </div>
